1f9d5b2d66d0a8cc6e080d1b2c2f1f71666a7853
Igor Ming de Mesquita <igor.ming@gmail.com>
2021-09-24 00:15:00 -0300

    configure firebase admob

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+// import {BannerAd, BannerAdSize, TestIds} from '@react-native-firebase/admob';
 import type {ReactNode} from 'react';
 import React from 'react';
 import {SafeAreaView, StatusBar, Text, useColorScheme} from 'react-native';
@@ -11,6 +12,19 @@ const App: () => ReactNode = () => {
     <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <Text>This is the app</Text>
+      {/* <BannerAd
+        unitId={TestIds.BANNER}
+        size={BannerAdSize.SMART_BANNER}
+        requestOptions={{
+          requestNonPersonalizedAdsOnly: true,
+        }}
+        onAdLoaded={() => {
+          console.log('Advert loaded');
+        }}
+        onAdFailedToLoad={error => {
+          console.error('Advert failed to load: ', error);
+        }}
+      /> */}
     </SafeAreaView>
   );
 };
