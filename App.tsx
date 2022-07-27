5597b5a7b6889e6de208e7be3dbad38f23000fa3
Igor Ming <igor.ming@gmail.com>
2022-07-26 21:29:21 -0300

    small change into app.tsx file

--- App.tsx
+++ App.tsx
@@ -3,12 +3,12 @@ import { BannerAd, BannerAdSize, TestIds } from '@react-native-firebase/admob';
 import { NavigationContainer } from '@react-navigation/native';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import React, { ReactNode, useLayoutEffect, useMemo } from 'react';
+import React, { ReactNode, useLayoutEffect } from 'react';
 import { Platform, StatusBar } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
-import { default as theme } from './theme.json';
-import RootNavigator from './src/navigators/root-navigator';
 import adIds from './firebase.json';
+import RootNavigator from './src/navigators/root-navigator';
+import { default as theme } from './theme.json';
 
 function getAdIdByOS() {
   return Platform.select({
@@ -22,11 +22,6 @@ const App: () => ReactNode = () => {
     SplashScreen.hide();
   }, []);
 
-  const adUnitId = useMemo(
-    () => (__DEV__ ? TestIds.BANNER : getAdIdByOS()),
-    [],
-  );
-
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
@@ -37,16 +32,10 @@ const App: () => ReactNode = () => {
         </NavigationContainer>
       </ApplicationProvider>
       <BannerAd
-        unitId={adUnitId}
+        unitId={__DEV__ ? TestIds.BANNER : getAdIdByOS()}
         size={BannerAdSize.SMART_BANNER}
         requestOptions={{
           requestNonPersonalizedAdsOnly: true,
-        }}
-        onAdLoaded={() => {
-          console.log('Advert loaded');
-        }}
-        onAdFailedToLoad={(error: Error) => {
-          console.error('Advert failed to load: ', error);
         }}
       />
     </>
